81380b7ef6d37cade3dd15cab5b60ed19f01e540
BORJA_DUYFIT\dudyp <borjadudyfit@gmail.com>
2021-12-10 13:27:25 +0100

    Enumerados Control de flujo

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MediaAdapter.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MediaAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myfirstapplication.MediaItem.Type
 
 open class MediaAdapter(private val items: List<MediaItem>) :
         RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
@@ -26,17 +27,24 @@ open class MediaAdapter(private val items: List<MediaItem>) :
         return holder.bind(item)
     }
 
-    //Devolver el numero de itmes que tiene nuestro MediaAdapter
+    //Devolver el numero de items que tiene nuestro MediaAdapter
     override fun getItemCount() = items.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val title: TextView = itemView.findViewById(R.id.mediaTitle)
         private val thumb: ImageView = itemView.findViewById(R.id.mediaThumb)
+        private val videIndicator: ImageView = itemView.findViewById(R.id.mediaVideIndicator)
 
         fun bind(mediaItem: MediaItem){
             title.text = mediaItem.title
             thumb.loadUrl(mediaItem.url)
             //Glide.with(thumb).load(mediaItem.url).into(thumb)
+
+            videIndicator.visibility = when(mediaItem.type){
+                Type.PHOTO -> View.INVISIBLE
+                Type.VIDEO -> View.VISIBLE
+
+            }
 
             itemView.setOnClickListener {
                 toastView(mediaItem.title)
